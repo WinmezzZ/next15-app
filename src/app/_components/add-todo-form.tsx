52f7d66bcd8a9e189b21414ab48b03478d9c09be
WinmezzZ <winme0308@gmail.com>
2024-11-28 22:22:35 +0800

    1

--- src/app/_components/add-todo-form.tsx
+++ src/app/_components/add-todo-form.tsx
@@ -5,21 +5,13 @@ import { addTodoSchema } from '../_actions/schema';
 import { addTodoAction } from '../_actions';
 import { Input } from '@/components/ui/input';
 import { Button } from '@/components/ui/button';
-import { useForm, useFormState } from 'react-hook-form';
+import { useForm } from 'react-hook-form';
 import { z } from 'zod';
-import { Form, FormControl, FormField, FormItem, FormLabel, FormMessage } from '@/components/ui/form';
-import { useEffect } from 'react';
+import { Form, FormControl, FormField, FormItem, FormMessage } from '@/components/ui/form';
+import { Loader2 } from 'lucide-react';
 
 const initialValues: z.infer<typeof addTodoSchema> = {
   newTodo: '',
-};
-
-const initialState = {
-  errors: {
-    firstName: undefined,
-    lastName: undefined,
-  },
-  message: undefined,
 };
 
 export function AddTodoForm() {
@@ -28,35 +20,27 @@ export function AddTodoForm() {
     defaultValues: initialValues,
   });
 
-  const [state, formAction] = useFormState(addTodoAction, initialState);
-
-  useEffect(() => {
-    console.log(state?.errors);
-    if (Array.isArray(state?.errors)) {
-      // Check if state.errors is an array before iterating
-      state.errors.forEach(error => {
-        form.setError(error.field, { message: error.message });
-      });
-    }
-  }, [state?.errors]);
+  const { control, formState } = form;
 
   return (
     <Form {...form}>
-      <form onSubmit={form.handleSubmit(addTodoAction)} className="space-y-8">
+      <form onSubmit={form.handleSubmit(addTodoAction)} className="flex gap-2">
         <FormField
-          control={form.control}
+          control={control}
           name="newTodo"
           render={({ field }) => (
             <FormItem>
-              <FormLabel>First Name</FormLabel>
               <FormControl>
                 <Input {...field} />
               </FormControl>
-              <FormMessage>{form.formState.errors.newTodo?.message}</FormMessage>
+              <FormMessage>{formState.errors.newTodo?.message}</FormMessage>
             </FormItem>
           )}
         />
-        <Button type="submit">Add</Button>
+        <Button type="submit" disabled={formState.isSubmitting}>
+          {formState.isSubmitting && <Loader2 className="w-4 h-4 animate-spin" />}
+          Add
+        </Button>
       </form>
     </Form>
   );
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -11,7 +11,7 @@ export default async function Home() {
 
   return (
     <div className="p-4 flex flex-col gap-2 items-center w-[400px] mx-auto">
-      <AddTodoForm todos={todos} />
+      <AddTodoForm />
       <ul className="w-full space-y-2 mt-4">
         {todos.map(todo => (
           <TodoItem key={todo.id} todo={todo} />
